adde9320e5921a94d0e2580589b6087c2bc8053f
Dave Irwin <david@redirwin.com>
2019-08-15 14:44:39 -0700

    Edited recipes schema.

--- data/seeds/001-recipes.js
+++ data/seeds/001-recipes.js
@@ -1,13 +1,28 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("table_name")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("table_name").insert([
+        {
+          id: 1,
+          name: "Peanut Butter Sandwich",
+          instructions:
+            "Spread peanut butter between two slices of bread. Cut into four precisely shaped triangles before serving to your child."
+        },
+        {
+          id: 2,
+          name: "Ham Sandwich",
+          instructions:
+            "Spread mayo and mustard between two slices of bread. Add ham, lettuce, and sliced tomatoes. Throw it in the trash and make a peanut butter sandwich instead."
+        },
+        {
+          id: 3,
+          name: "Tostado",
+          instructions:
+            "Spread warm refried beans on a crunchy corn shell. Add grated cheese, chopped lettuce, diced tomatoes, and salsa. Watch as your child breaks it into tiny bits and spreads the beans all over her face."
+        }
       ]);
     });
 };
