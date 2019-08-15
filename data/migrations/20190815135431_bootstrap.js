1707980c63947bb7a6fa69f6707029ea16d43f33
Dave Irwin <david@redirwin.com>
2019-08-15 14:20:01 -0700

    Created tables for recipe book and ran migration.

---
+++ data/migrations/20190815135431_bootstrap.js
@@ -0,0 +1,47 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("recipes", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 255)
+        .notNullable()
+        .unique();
+      tbl
+        .string("instructions", 5000)
+        .notNullable()
+        .unique();
+    })
+    .createTable("ingredients", tbl => {
+      tbl.increments();
+      tbl
+        .string("name", 255)
+        .notNullable()
+        .unique();
+    })
+    .createTable("recipe_ingredients", tbl => {
+      tbl.increments();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
+};
--- knexfile.js
+++ knexfile.js
@@ -11,10 +11,9 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     },
-    // sqlite will not enforce Foreign Keys by default
     pool: {
       afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done); // turns on the FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // enforces foreign keys in sqlite
       }
     }
   }
